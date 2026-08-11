166fa7777cf7a60a8e10b3adc923d06004577836
Archit Jain <>
2026-08-11 18:32:30 +0530

    Updates

---
+++ registerSW.js
@@ -0,0 +1,1 @@
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/https://github.com/architjain-12/Expense-Tracker/sw.js', { scope: '/https://github.com/architjain-12/Expense-Tracker/' })})}
